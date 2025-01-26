321d34162dd0102999cb4e802e199caf3209e63e
Drew Cook <dc333@usf.edu>
2025-01-26 12:37:12 -0500

    Task 2 - Product Details

--- cc_1/cc_1.js
+++ cc_1/cc_1.js
@@ -1,3 +1,4 @@
+//Task 1
 // First, ask the user for their name and save it as a string
 let employeeName = "John";
 
@@ -11,3 +12,18 @@ var isActive = true;
 console.log(`Employee name: Value: ${employeeName} Type: ${typeof employeeName}
 Employee ID: Value: ${employeeID} Type: ${typeof employeeID}
 Active: Value: ${isActive} Type: ${typeof isActive}`);
+
+//Task 2
+//Declare variable productName using let, string value
+let productName = "Table";
+
+//Declare variable price with const, numerical value
+const productPrice = 150;
+
+//Declare an availability variable, boolean value
+var isAvailable = true;
+
+//Log the variables and their types
+console.log(`Product Name: Value: ${productName} Type: ${typeof productName}
+Product Price: Value: ${productPrice} Type: ${typeof productPrice}
+Availability: Value: ${isAvailable} Type: ${typeof isAvailable}`);
