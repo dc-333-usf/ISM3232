39f1ff9b42e6f2e4853cd57ece12a628c34c4731
Drew Cook <dc333@usf.edu>
2025-01-26 12:25:59 -0500

    Task 1 - Employee Information

--- cc_1/cc_1.js
+++ cc_1/cc_1.js
@@ -1,10 +1,13 @@
 // First, ask the user for their name and save it as a string
-let employeeName = prompt(String("What is your name?"));
+let employeeName = "John";
 
 //Then, we must assign an employee ID.
-const employeeID = 102870
+const employeeID = 102870;
 
 //Last, we'll create a variable for activity status.
-var isActive = true
+var isActive = true;
 
-console.log("Type: ${typeof employeeName}, Value: ${employeeName}")
+//Log the variables and their types to the console.
+console.log(`Employee name: Value: ${employeeName} Type: ${typeof employeeName}
+Employee ID: Value: ${employeeID} Type: ${typeof employeeID}
+Active: Value: ${isActive} Type: ${typeof isActive}`);
