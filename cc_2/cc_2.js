dbc58def25bbb0cb710d9240e83a635d16cb9697
Drew Cook <dc333@usf.edu>
2025-01-26 21:02:31 -0500

    Task 2 - Student Scores

--- cc_2/cc_2.js
+++ cc_2/cc_2.js
@@ -6,3 +6,11 @@ products.pop();
 console.log(products);
 //Well now what was the point of that...?
 
+//Task 2
+//Create an array of scores, update a specific value, find average
+let scores = [71, 55, 89, 93, 99, 81];
+scores[1] = 75;
+let sum = scores.reduce((current, updated) => current + updated, 0);
+let average = sum / scores.length;
+console.log(`Scores: ${scores}
+Average: ${average}`);
