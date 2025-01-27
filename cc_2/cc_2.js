9b7d3f9442e45047e721c27970f4cfc472f72e83
Drew Cook <dc333@usf.edu>
2025-01-26 20:17:17 -0500

    Task 1 - Store Inventory

---
+++ cc_2/cc_2.js
@@ -0,0 +1,8 @@
+//Task 1
+//Create an array: add, remove, and log the list.
+let products = ["Table", "Chair", "Couch", "Mirror", "Lamp"];
+products.push("Bed");
+products.pop();
+console.log(products);
+//Well now what was the point of that...?
+
